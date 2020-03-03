809b10703d35700dd5666a120d9ec990027557a2
[derekCormack] <[dcormack.dc@gmail.com]>
2020-03-02 17:43:22 -0700

    add event listner  addition only

--- 01-getting-started/src/scripts/functions.js
+++ 01-getting-started/src/scripts/functions.js
@@ -1,35 +1,37 @@
 // --------------------- The functions FUNCTION -----------------------
 
-var num1;
-var num2;
-var calcTotal;
+// var num1 = 0;
+// var num2 = 0;
+// var calcTotal = 0;
 
+console.log("hello world")
 
 const functions = {
     
-    size: (num) => {
-        if (num < 10) return "small";
-        if (num < 20) return "medium";
-        return "large";
-    },
+    // size: (num) => {
+    //     if (num < 10) return "small";
+    //     if (num < 20) return "medium";
+    //     return "large";
+    // },
 
     add: (num1, num2) => {
-        calcTotal = num1 + num2;
-        document.getElementById("result").innerHTML = "Paragraph changed!";
+        return  calcTotal =Number(num1) + Number(num2);        
+        
     },
 
     subtract: (num1, num2) => {
-        return calcTotal = num1 - num2;
+        return calcTotal = Number(num1) - Number(num2); 
+        
     },
 
-    multipy (num1, num2) {
-        return calcTotal = num1 * num2;
+    multiply: (num1, num2) => {
+        return calcTotal = Number(num1) * Number(num2); 
     },
 
-    divide(num1, num2) {
-        return calcTotal = num1 / num2;
+    divide: (num1, num2) => {
+        return calcTotal = Number(num1) / Number(num2); 
     },
 
-};
+ };
 
 export default functions;
--- 01-getting-started/src/scripts/functions.test.js
+++ 01-getting-started/src/scripts/functions.test.js
@@ -1,12 +1,36 @@
 import functions from './functions'
 
- test('this is where we Check the calcs', () => {
-     expect(functions.addNums(5,5)).toEqual(10);
-     });
+test("check add", () => {
+    expect(functions.add(1,1)).toEqual(2); 
+    expect(functions.add(2,2)).toEqual(4); 
+    expect(functions.add("2","2")).toEqual(4);
+});
+
+test("subtract", () => {
+    expect(functions.subtract(3,2)).toEqual(1);
+    expect(functions.subtract(10,7)).toEqual(3);
+    expect(functions.subtract("5","3")).toEqual(2);
+
+});
+
+test("divide", () => {
+    expect(functions.divide(8,2)).toEqual(4); 
+    expect(functions.divide("10","5")).toEqual(2); 
+});
+ 
+test("multiply", () => {
+    expect(functions.multiply(3,2)).toEqual(6); 
+    expect(functions.multiply("10","5")).toEqual(50); 
+});
+
+
+
+// expect(functions.subtract(1,1)).toEqual(2); 
+// expect(functions.multiply(4,2)).toEqual(2); 
+// expect(functions.divide(4,2)).toEqual(8); 
+
 
  
-
-
 //     expect(functions.add(1,1)).toBe(2); 
 //     expect(functions.makePizza("mushroom","pepperoni")).
 //     toBe("mushroom pepperoni"); 
@@ -19,6 +43,4 @@ import functions from './functions'
 //     expect(functions.ifBeer(8)).toBe("Coffee time");
 //     expect(functions.addBack(["wildRose", "kokanee", "richardsRed", "blacksmith"]))
 //     .toEqual(["wildRose", "kokanee", "richardsRed", "blacksmith","coors"])
-    //  expect(functions.beerSplice(["wildRose", "kokanee", "richardsRed", "blacksmith"], "pop")).toEqual(["pop", "kokanee", "richardsRed", "blacksmith"])
- 
- 
+//  expect(functions.beerSplice(["wildRose", "kokanee", "richardsRed", "blacksmith"], "pop")).toEqual(["pop", "kokanee", "richardsRed", "blacksmith"])
