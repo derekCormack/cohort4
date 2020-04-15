9463e327cace5d2402c2fff2880a8ed5f51d05c8
[derekCormack] <[dcormack.dc@gmail.com]>
2020-04-15 15:28:03 -0600

    revise gitnore, delete cohort 4 on git, push with gitnor

--- DOM TEST/script.js
+++
@@ -1,9 +0,0 @@
-
-
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput")
-
-button.addEventListener("mouseleave", function() {
-    console.log("CLICK!!!!");
-
-})
