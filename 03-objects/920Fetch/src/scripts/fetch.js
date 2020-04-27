0857521f4008ce3c69e8c8c7aa0c26e7586da0da
[derekCormack] <[dcormack.dc@gmail.com]>
2020-04-27 08:24:23 -0600

    TDD fetch, connection error issues

--- 03-objects/920Fetch/src/scripts/fetch.js
+++ 03-objects/920Fetch/src/scripts/fetch.js
@@ -1,23 +1,97 @@
+//Referring a node-fetch package which we installed for calling APIs
+//node-fetch behaves like Http client module: https://www.npmjs.com/package/node-fetch
+const fetch = require('node-fetch');
+
+//declare function fetch(input: RequestInfo, init?: RequestInit): Promise<Response>;
 
 const functions = {
 
-    url: 'https://jsonplaceholder.typicode.com/users',
-
+    url: 'https://jsonplaceholder.typicode.com/users',  //url is like a Endpoint of API
+   
     getFirstName(data) {
         return data.name;
+    },
+
+    getAllFirstNames(data) {
+        //map() -> takes 3 arguments, 1st object, 2nd index, 3rd whole data
+        return data.map((d, i, x) => d.name); //d = d would iterate thru's all objects, i=index (i)
+    },
+
+    showDelayProblem() {
+        console.log('One');
+        setTimeout(() => {          // Simulates a fetch
+            console.log("Two");
+        }, 1 * 1000);
+        console.log('Three');       // We have a problem Huston
+    },
+
+    async getUsers() {
+        //this method will fetch all users i.e. the data which we hardcoded
+        const response = await fetch(functions.url); // this.http.get(url);
+        //const data = response.json();
+
+        if(response.ok) {
+            const data = await response.json();
+            return data;
+        }
+
+        //No need to use .then() or resolve the promise!!! since fetch() handles internally...
+    },
+    
+    //https://web.dev/promises/             sample from.
+    getUsersWithActualPromiseAPICall() {
+        // Return a new promise.
+        return new Promise(function(resolve, reject) {
+            // Do the usual XHR stuff
+            var req = new XMLHttpRequest();
+
+            //XMLHttpRequest - this has methods which we need to call,
+            //0 method - open()
+            //1st method is onload - which is responsible for resolving and rejecting the promise based on what you get from server
+            //2nd method is onerror - which is responsible for error handling
+            //3rd method is send - responsible for communicating with server
+            req.open('GET', functions.url);
+        
+            // req.onload(() => {
+            //     if (req.status == 200) {
+            //         // Resolve the promise with the response text
+            //         resolve(req.response);
+            //       }
+            //       else {
+            //         // Otherwise reject with the status text
+            //         // which will hopefully be a meaningful error
+            //         reject(Error(req.statusText));
+            //       }
+            // })
+
+            req.onload = function() {
+              // This is called even on 404 etc
+              // so check the status
+              if (req.status == 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+              }
+              else {
+                // Otherwise reject with the status text
+                // which will hopefully be a meaningful error
+                reject(Error(req.statusText));
+              }
+            };
+        
+            // Handle network errors
+            req.onerror = function() {
+                //you will do something on UI when you get error
+              reject(Error("Network Error"));
+            };
+        
+            // Make the request
+            req.send();
+          });  
     }
-
-    //how do you call an API?
-
-    //with the help of Promises
-
 }
 
- 
 export default functions;
-   
-
-    // async showDelaySolution() {
+      // async showDelaySolution() {
     //     try {
     //         console.log('One');
     //         const value = await                 // Simulate fetch
